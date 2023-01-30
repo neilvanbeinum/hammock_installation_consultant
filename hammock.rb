4e819de258c9e43403b09e8b25ba989fbf68b34f
vvveebs <1370570+vvveebs@users.noreply.github.com>
2023-01-30 20:37:09 +0700

    Most basic

---
+++ hammock.rb
@@ -0,0 +1,12 @@
+# What is the hammock length?
+
+# Two sets of attachment points
+
+# Hammock must be attached to one point from each set
+
+# How many combinations are there?
+
+RAIL_LENGTH_A = 12
+RAIL_LENGTH_B = 8
+
+puts RAIL_LENGTH_A * RAIL_LENGTH_B
